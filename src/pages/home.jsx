6277aba40430256170359797f7f045ea840422b0
Yusin <sunkatabeckama@gmail.com>
2024-06-24 14:31:24 +0200

    Home page component

---
+++ src/pages/home.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import './home.css'
+import Header from '../components/header/header'
+
+
+export default function Home() {
+    const { id } = useParams()
+    const [pageLoading, setPageLoading] = useState(true)
+    const [pageNotFound, setPageNotFound] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setPageLoading(true)
+            } catch (error) {
+                if (error.message === 'User not found') {
+                    setPageNotFound(true)
+                }
+            } finally {
+                setPageLoading(false)
+            }
+        }
+        fetchData()
+    }, [id])
+
+    if (pageNotFound) {
+        return <Navigate to="/404" />
+    }
+
+    return (
+        <main>
+            <div className="ss-profile">
+                <Header userId={Number(id)} className="ss-header" />
+            </div>
+        </main>
+    )
+}
